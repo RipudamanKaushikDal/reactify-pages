41f9ee9c81e7101b15fafe298fdd753d03da6916
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2020-11-07 01:27:52 -0400

    Added Swipe gestures to banner and multiple colors to banner buttons

--- src/components/CFOD/Banner.js
+++ src/components/CFOD/Banner.js
@@ -3,43 +3,53 @@ import React from 'react'
 import './Banner.css'
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
+import { virtualize } from 'react-swipeable-views-utils';
+
+
 
 function Banner({data}) {
 
-  
+    const propData = data;
 
     // Library to add auto-carousel effect to banner
+    const AutoPlaySwipeableViews = autoPlay(virtualize(SwipeableViews));
 
-    const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+    // Function to render slides
+    const BannerData = () =>
+        (
+
+        propData.map((selectedData,index) => (
+            <header className="banner" style={{backgroundImage:`url(${selectedData.image})`}} key={index}>
+                <div className="banner_contents">
+                    <h1 className="banner_title">
+                        {selectedData.title}
+                    </h1>
+
+                    <div className="banner_description">
+                        <h4>{selectedData.description}</h4>
+                    </div>
+
+                    <div >
+                        <button className={`banner_button ${index%2===0 && "banner_alternate"}`} >Start Now</button>
+                    </div>                
+                </div>
+                <div className="banner--fadebottom"></div>
+        </header> 
+
+
+        ))
+    )
+
 
 
     return (
 
         // Render various banner components with a carousel effect
-        <AutoPlaySwipeableViews interval={4000} enableMouseEvents={true} >
-        {data.map(selectedData => (
-                <header className="banner" style={{backgroundImage:`url(${selectedData.image})`}} key={selectedData.title}>
-                    <div className="banner_contents">
-                        <h1 className="banner_title">
-                            {selectedData.title}
-                        </h1>
-
-                        <div className="banner_description">
-                            <h4>{selectedData.description}</h4>
-                        </div>
-
-                        <div >
-                            <button className="banner_button" >Start Now</button>
-                        </div>                
-                    </div>
-                    <div className="banner--fadebottom"></div>
-            </header> 
-        
-
-        ))}
-        
-    </AutoPlaySwipeableViews>
+        <AutoPlaySwipeableViews interval={4000} slideCount={propData.length} slideRenderer={BannerData} / >
+    
     )
 }
+
+
 
 export default Banner
